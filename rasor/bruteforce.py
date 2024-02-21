6aeedd1d7952351983a6aba718f1aee3889acd1f
jung-benjamin <63796898+jung-benjamin@users.noreply.github.com>
2024-02-21 18:24:34 +0100

    Add bruteforce module
    The module contains classes for running combination based brute force
    ratio selection.

---
+++ rasor/bruteforce.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python3
+"""Select isotopic ratios by evaluating all combinations."""
+
+from itertools import combinations
+
+import numpy as np
+
+from .metrics import MaxLikelihoodUncertainty
+from .sampling import SamplerFactory
+
+
+class SolubilityMatrix:
+    """Calculate metric on a grid of test points."""
+
+    def __init__(self, metric, test_points):
+        """Define the test space and set the metric."""
+        test_point_dispatcher = {
+            np.ndarray: self.set_test_points,
+            dict: self.sample_test_points
+        }
+        self.metric = metric
+        t = type(test_points)
+        test_point_dispatcher[t](test_points)
+
+    def set_test_points(self, tp):
+        """Set the test point array."""
+        self.test_points = tp
+
+    def sample_test_points(self, kwarg_dict):
+        """Create a sampler and create the test point array."""
+        self.sampler = SamplerFactory().get_sampler(**kwarg_dict)
+        self.set_test_points(self.sampler())
+
+    def fill(self):
+        """Evaluate the metric on each test_point."""
+        self.matrix = np.empty(self.test_points.shape[0])
+        for i, tp in enumerate(self.test_points):
+            self.matrix[i] = self.metric(tp)
+
+    @classmethod
+    def from_dict(cls, d):
+        """Construct class from parameters in a dictionary"""
+        metric = MaxLikelihoodUncertainty.from_dict(d=d)
+        return cls(metric=metric, test_points=d['Test_points'])
+
+
+def get_metric(ratios, param_dict):
+    param_dict['Problem'].update({'ratios': ratios})
+    return MaxLikelihoodUncertainty.from_dict(param_dict)
+
+
+def evaluate_solubility_matrix(ratios, test_points, metric_params):
+    """Create and fill the solubility matrix"""
+    metric = get_metric(ratios, metric_params)
+    solu = SolubilityMatrix(metric=metric, test_points=test_points)
+    solu.fill()
+    return solu.matrix
+
+
+class Minotaur:
+    """Iterate through ratio combinations with brute force."""
+
+    combo_length = 2
+
+    def __init__(self, ratios, test_points, metric_params):
+        self.ratios = ratios
+        self.test_points = test_points
+        self.metric_params = metric_params
+
+    def combinations(self):
+        """Iterator over all combinations of ratios."""
+        return combinations(self.ratios, r=self.combo_length)
+
+    def _scan(self):
+        matrices = {}
+        for r in self.combinations():
+            matrices[','.join(r)] = evaluate_solubility_matrix(
+                ratios=r,
+                test_points=self.test_points,
+                metric_params=self.metric_params)
+        return list(matrices.keys()), list(matrices.values())
+
+    def _scan_multiproc(self, num_proc):
+        raise NotImplementedError
+
+    def fight(self, num_proc=1):
+        """Calculate solubility matrix for each ratio combination."""
+        if num_proc > 1:
+            keys, solubility = self._scan_multiproc(num_proc=num_proc)
+        else:
+            keys, solubility = self._scan()
+        return keys, solubility
