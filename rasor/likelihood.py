739865a5674e6fea20d3bc5f754c223734064691
jung-benjamin <63796898+jung-benjamin@users.noreply.github.com>
2024-02-21 13:51:17 +0100

    Add likelihood module

---
+++ rasor/likelihood.py
@@ -0,0 +1,98 @@
+#! /usr/bin/env python3
+"""Likelihood function models"""
+
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class UncertaintyModel(ABC):
+    """Model for relative uncertianty of isotopic ratios."""
+
+    @abstractmethod
+    def _uncertainty(self, ratio):
+        """Calculate the relative uncertainty given a raito"""
+        ...
+
+    def __call__(self, ratio):
+        """Calculate relative uncertainty and multiply with ratio."""
+        return self._uncertainty(ratio) * ratio
+
+
+class ConstantUncertainty(UncertaintyModel):
+
+    def __init__(self, value=0.1):
+        """Set value of constant relative uncertainty."""
+        self.value = value
+
+    def _uncertainty(self, ratio):
+        return self.value
+
+
+class UncertaintyFactory:
+
+    _uncertainty = {
+        'constant': ConstantUncertainty,
+    }
+
+    def get_model(self, model, **kwargs):
+        """Create an instance of an uncertainty model."""
+        model = self._uncertainty.get(model)
+        if not model:
+            raise ValueError(model)
+        return model(**kwargs)
+
+
+class LikelihoodModel(ABC):
+    """Approximate the joint probabilitiy density function."""
+
+    def __init__(self, surrogates, test_point, uncertainty_model, **kwargs):
+        """Set the test point and the surrogate models."""
+        self.surrogates = surrogates
+        self.test_point = test_point
+        self.uncertainty = UncertaintyFactory().get_model(
+            uncertainty_model, **kwargs)
+        self.calc_mu()
+        self.calc_sigma()
+
+    def calc_mu(self):
+        """Evaluate the surrogates on the test point."""
+        self.mu = [sur(self.test_point) for sur in self.surrogates]
+
+    def calc_sigma(self):
+        self.sigma = [self.uncertainty(m) for m in self.mu]
+
+    @abstractmethod
+    def pdf(self, x):
+        """Probability distribution function for variable"""
+        ...
+
+    def joint_pdf(self, x):
+        """Joint probability distribution function
+        
+        Assumes all variables are independent and the joint probability
+        is the product of each probability distribution function.
+        """
+        prob = 1
+        for func, mu, sigma in zip(self.surrogates, self.mu, self.sigma):
+            prob *= self.pdf(func(x), mu, sigma)
+        return prob
+
+    def __call__(self, x):
+        """Calculate the joint probability for x
+        
+        Parameters
+        ----------
+        x : np.ndarray(float)
+            Values for which to calculate the joint probability.
+            Shape is (num, dim) where num is the number of points
+            and"""
+        return self.joint_pdf(x)
+
+
+class GaussianLikelihood(LikelihoodModel):
+    """Likelihood function with normal distributions."""
+
+    def pdf(self, x, mu, sigma):
+        """Normal probability distribution function."""
+        return np.exp(-(x - mu)**2 / (2 * (sigma**2)))
