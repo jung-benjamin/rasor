f0b9685a5190786e097fe6c7b3950665596f75c0
jung-benjamin <63796898+jung-benjamin@users.noreply.github.com>
2024-02-27 15:46:33 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name='rasor',
-      version='0.1.0',
+      version='0.2.0',
       author='Benjamin Jung',
       license='BSD-3-Clause',
       packages=find_packages(include=['rasor', 'rasor.*']),
