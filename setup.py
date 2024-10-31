c5f133a8ce5fb8c0551f5d0789197287b4e37c33
jung-benjamin <63796898+jung-benjamin@users.noreply.github.com>
2024-10-31 13:34:53 +0100

    Restructure the install procedure
    Adds a pyproject.toml and a setup.cfg file to be more in line with the
    current practices for installing packages.

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 
 from setuptools import find_packages, setup
 
-setup(name='rasor',
-      version='0.2.0',
-      author='Benjamin Jung',
-      license='BSD-3-Clause',
-      packages=find_packages(include=['rasor', 'rasor.*']),
-      description='Algorithms for selecting suitable isotopic ratios.',
-      entry_points={
-          'console_scripts': [
-              'run_ratio_selection=rasor.command_line:run_ratio_selection',
-              'run_pre_selection=rasor.command_line:run_pre_selection',
-              'run_post_processing=rasor.command_line:run_post_processing'
-          ]
-      })
+# setup(name='rasor',
+#       version='0.2.0',
+#       author='Benjamin Jung',
+#       license='BSD-3-Clause',
+#       packages=find_packages(include=['rasor', 'rasor.*']),
+#       description='Algorithms for selecting suitable isotopic ratios.',
+#       entry_points={
+#           'console_scripts': [
+#               'run_ratio_selection=rasor.command_line:run_ratio_selection',
+#               'run_pre_selection=rasor.command_line:run_pre_selection',
+#               'run_post_processing=rasor.command_line:run_post_processing'
+#           ]
+#       })
+setup()
